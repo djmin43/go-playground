afdd8b5388905dd9233650db051904b2636e795d
jay <dj.min43@gmail.com>
2021-08-03 13:09:05 +0900

    sass

--- src/common/components/layout/Navbar.tsx
+++ src/common/components/layout/Navbar.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div className="navbar">
-      navbar
+      <div>modal</div>
+      <div>Today&apos;s deals</div>
+      <div>Customer Service</div>
+      <div>About us</div>
     </div>
   )
 }
