0685eb7d876a61153d36a5f48e6393d29f295fae
jay <dj.min43@gmail.com>
2021-08-13 11:01:05 +0900

    remove redux

--- src/common/components/about-us/AboutUs.tsx
+++ src/common/components/about-us/AboutUs.tsx
@@ -1,35 +1,10 @@
 import React, { ReactElement } from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../../redux/store'
-import { useDispatch } from 'react-redux'
-import { nanoid } from '@reduxjs/toolkit'
-import { postNewMovie } from '../../../redux/reducers/postSlice'
 
 const AboutUs = (): ReactElement => {
-  const reduxValue = useSelector((state: RootState) => state.value)
-
-  const dispatch = useDispatch()
-
-  const handlePostNewMovie = () => {
-    dispatch(
-      postNewMovie({
-        id: nanoid(),
-        title: 'New Movie',
-        genre: 'new genre',
-      })
-    )
-  }
 
   return (
     <div>
-      asdf
-      {reduxValue.map(({id, title, genre}) => 
-        <div key={id}>
-          <p>{title}</p>
-          <p>{genre}</p>
-        </div>
-      )}
-      <button onClick={handlePostNewMovie}>add a new movie</button>
+      about us page
     </div>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import '../../styles/globals.scss'
-import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
-import store from '../redux/store/index'
 import Layout from '../common/components/layout/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </Provider>
   )
 }
 
