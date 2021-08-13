193b632d75608f5ecabc08aa33cfc0605160557b
jay <dj.min43@gmail.com>
2021-08-13 11:10:19 +0900

    aphrodite

--- src/common/components/about-us/AboutUs.tsx
+++ src/common/components/about-us/AboutUs.tsx
@@ -1,12 +1,19 @@
 import React, { ReactElement } from 'react'
+import { StyleSheet, css } from 'aphrodite'
 
 const AboutUs = (): ReactElement => {
 
   return (
-    <div>
+    <div className={css(styles.aboutUs)}>
       about us page
     </div>
   )
 }
+
+const styles = StyleSheet.create({
+  aboutUs: {
+    height: '80vh'
+  }
+})
 
 export default AboutUs
