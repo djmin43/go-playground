37abd2f31af5231438aadb2a3d72e414fe3580ff
jay <dj.min43@gmail.com>
2021-08-15 22:56:29 +0900

    remove test tsx

--- src/common/components/todays-deal/TodaysDeal.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import TodaysDeal from './TodaysDeal';
-
-test('sample test', () => {
-  render(<TodaysDeal />);
-});
