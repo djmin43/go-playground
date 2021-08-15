e0707b15b4fd79b5481926ed7e5f0cc4ef63861d
jay <dj.min43@gmail.com>
2021-08-15 22:48:40 +0900

    rollback again

--- src/common/components/todays-deal/TodaysDeal.test.tsx
+++ src/common/components/todays-deal/TodaysDeal.test.tsx
@@ -4,6 +4,5 @@ import { render } from '@testing-library/react';
 import TodaysDeal from './TodaysDeal';
 
 test('sample test', () => {
-  const { container } = render(<TodaysDeal />);
-  expect(container.firstChild).toBeVisible();
+  render(<TodaysDeal />);
 });
