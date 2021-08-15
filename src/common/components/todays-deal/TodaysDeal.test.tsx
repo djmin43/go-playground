176cb292b1ba9d52c8e04781f8d901ed001ae4fb
jay <dj.min43@gmail.com>
2021-08-15 22:38:33 +0900

    rollback

--- src/common/components/todays-deal/TodaysDeal.test.tsx
+++ src/common/components/todays-deal/TodaysDeal.test.tsx
@@ -1,1 +1,9 @@
-import React from 'react';
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import TodaysDeal from './TodaysDeal';
+
+test('sample test', () => {
+  const { container } = render(<TodaysDeal />);
+  expect(container.firstChild).toBeVisible();
+});
