78cbf466b8a9368ee8388eb0f56185dcc16d7a4d
jay <dj.min43@gmail.com>
2021-08-16 10:50:26 +0900

    testing works

---
+++ src/common/components/todays-deal/TodaysDeal.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TodaysDeal from './TodaysDeal';
+
+it('testing a test run', () => {
+  render(<TodaysDeal />);
+});
