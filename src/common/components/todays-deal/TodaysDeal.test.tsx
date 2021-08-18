0b774fb3af1530ff294b137c5e2ebd696fc79598
jay <dj.min43@gmail.com>
2021-08-18 18:51:51 +0900

    remove fetch image test

--- src/common/components/todays-deal/TodaysDeal.test.tsx
+++ src/common/components/todays-deal/TodaysDeal.test.tsx
@@ -27,39 +27,9 @@ const server = setupServer(
   })
 )
 
-// beforeAll(() => server.listen())
-// afterAll(() => server.close())
-
 afterEach(() => cleanup())
 
 test('testing a test run', async () => {
   await render(<TodaysDeal />)
   screen.debug(screen.getByText('images'))
 });
-
-test('handles fetching images', async () => {
-  await render(<TodaysDeal />)
-  // server.use(
-  //   await rest.get('https://picsum.photos/v2/list', (req, res, ctx) => {
-  //     return res(ctx.status(200),
-  //     ctx.json({
-  //       data: [
-  //         {
-  //           id: 1,
-  //           author: 'tester1',
-  //           download_url: 'tester_url1',
-            
-  //         },
-  //         {
-  //           id: 2,
-  //           author: 'tester2',
-  //           download_url: 'tester_url2',
-  //         },
-  //       ],
-  //     }))
-  //   })
-  // )
-
-  const image = await screen.findByTestId('images')
-  await screen.debug(image)
-})
