8c5662d22c7daee71f5b3b1c113b6ff8ef08d328
jay <dj.min43@gmail.com>
2023-08-19 13:32:08 +0900

    iteration with string repeat

---
+++ fundamentals/iteration/repeat.go
@@ -0,0 +1,7 @@
+package iteration
+
+import "strings"
+
+func Repeat(character string, sequence int) string {
+	return strings.Repeat(character, sequence)
+}
---
+++ fundamentals/iteration/repeat_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q, but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
+
+}
