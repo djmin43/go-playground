9b5ba511981f41d10e0d2a4eabe5c4d397652189
jay <dj.min43@gmail.com>
2023-08-20 19:02:14 +0900

    assert with no error

--- fundamentals/pointers-and-errors/wallet_test.go
+++ fundamentals/pointers-and-errors/wallet_test.go
@@ -14,9 +14,10 @@ func TestWallet(t *testing.T) {
 
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
+	t.Run("withdraw with funds", func(t *testing.T) {
 		wallet := Wallet{Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(10))
 	})
 
@@ -39,6 +40,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 		t.Errorf("got %s want %s", got, want)
 	}
 
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
 }
 
 func assertError(t testing.TB, got error, want error) {
