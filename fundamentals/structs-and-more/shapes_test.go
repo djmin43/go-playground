1de29d342feed11eda223dd979fd82b2f1282099
jay <dj.min43@gmail.com>
2023-08-20 16:00:15 +0900

    test tables

--- fundamentals/structs-and-more/shapes_test.go
+++ fundamentals/structs-and-more/shapes_test.go
@@ -13,7 +13,6 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
@@ -25,12 +24,27 @@ func TestArea(t *testing.T) {
 	t.Run("rectangles", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
 		checkArea(t, rectangle, 72.0)
-
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
 		checkArea(t, circle, 314.1592653589793)
 	})
+}
 
+func TestAreaTable(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
