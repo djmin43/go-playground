ec2fdfb1f9545a422590f3a035ffb056e6430869
jay <dj.min43@gmail.com>
2023-08-21 21:08:57 +0900

    add maps

--- fundamentals/maps/dictionary.go
+++ fundamentals/maps/dictionary.go
@@ -1,1 +1,5 @@
 package maps
+
+func Search(dictionary map[string]string, word string) string {
+	return dictionary[word]
+}
--- fundamentals/maps/dictionary_test.go
+++ fundamentals/maps/dictionary_test.go
@@ -1,1 +1,22 @@
 package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	dictionary := map[string]string{"test": "this is just a test"}
+
+	got := Search(dictionary, "test")
+	want := "this is just a test"
+
+	assertStrings(t, got, want)
+
+}
+
+func assertStrings(t testing.TB, got string, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, "test")
+	}
+}
