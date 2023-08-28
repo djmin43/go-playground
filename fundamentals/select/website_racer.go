51dd2051b43b6b49d2a19e664d2f2fb0f481a656
jay <dj.min43@gmail.com>
2023-08-28 20:16:28 +0900

    select work in progress

--- fundamentals/select/website_racer.go
+++ fundamentals/select/website_racer.go
@@ -1,1 +1,25 @@
 package _select
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	fmt.Printf("%q", aDuration)
+	fmt.Printf("%q", bDuration)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+}
--- fundamentals/select/website_racer_test.go
+++ fundamentals/select/website_racer_test.go
@@ -1,1 +1,16 @@
 package _select
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www.facebook.com"
+	fastURL := "http://www.quii.dev"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+
+}
