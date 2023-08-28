22021d0cc1e72b0002b7e489451689f0c1faaa2b
jay <dj.min43@gmail.com>
2023-08-28 14:09:00 +0900

    sleeper

--- fundamentals/mocking/countdown.go
+++ fundamentals/mocking/countdown.go
@@ -1,1 +1,48 @@
-package mocking
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct {
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+// syntax for method for Spy Sleeper struct
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+
+}
