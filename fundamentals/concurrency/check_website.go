ac4d57438b303f217bba1a9c80a7e69cbfd83119
jay <dj.min43@gmail.com>
2023-08-28 16:00:45 +0900

    go race condition

--- fundamentals/concurrency/check_website.go
+++ fundamentals/concurrency/check_website.go
@@ -1,12 +1,24 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) {
+			resultChannel <- result{u, wc(u)}
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		r := <-resultChannel
+		results[r.string] = r.bool
 	}
 
 	return results
