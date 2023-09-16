c5c7fbb3b3ca6a37e9b72d861883a3a49f66b98e
jay <dj.min43@gmail.com>
2023-09-17 08:02:44 +0900

    write counter test first

---
+++ fundamentals/sync/sync_test.go
@@ -0,0 +1,14 @@
+package sync
+
+import "testing"
+
+func TestCounter(t *testing.T) {
+	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+
+		if counter.Value() != 3 {
+			t.Errorf("got %d, want %d", counter.Value(), 3)
+		}
+	})
+}
