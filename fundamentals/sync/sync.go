c332d4742947af68b8c7fb945b6e8d9ee0469233
jay <dj.min43@gmail.com>
2023-09-17 08:22:04 +0900

    mutex vs channels

--- fundamentals/sync/sync.go
+++ fundamentals/sync/sync.go
@@ -1,13 +1,22 @@
 package sync
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
 func (c *Counter) Value() int {
 	return c.value
+}
+
+func NewCounter() *Counter {
+	return &Counter{}
 }
--- fundamentals/sync/sync_test.go
+++ fundamentals/sync/sync_test.go
@@ -1,19 +1,41 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := NewCounter()
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
 		assertCounter(t, counter, 3)
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+
 }
 
-func assertCounter(t testing.TB, got Counter, want int) {
+func assertCounter(t testing.TB, got *Counter, want int) {
 	t.Helper()
 	if got.Value() != want {
 		t.Errorf("got %d, want %d", got.Value(), want)
