30e861989a60e2b76945645965dd8ea871bf4fd9
jay <dj.min43@gmail.com>
2023-09-16 17:35:35 +0900

    reflection wip

---
+++ fundamentals/reflection/walk_test.go
@@ -0,0 +1,26 @@
+package reflection
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	expected := "Chris"
+	var got []string
+
+	x := struct {
+		Name string
+	}{expected}
+
+	walk(x, func(input string) {
+		got = append(got, input)
+	})
+
+	if len(got) != 1 {
+		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	}
+
+	if got[0] != expected {
+		t.Errorf("got %q, want %q", got[0], expected)
+
+	}
+
+}
