00bb31bb79452d2436f967ec3173632ec8717b08
jay <dj.min43@gmail.com>
2023-09-17 08:06:25 +0900

    write a simple test counter

---
+++ fundamentals/sync/sync.go
@@ -0,0 +1,13 @@
+package sync
+
+type Counter struct {
+	value int
+}
+
+func (c *Counter) Inc() {
+	c.value++
+}
+
+func (c *Counter) Value() int {
+	return c.value
+}
--- fundamentals/sync/sync_test.go
+++ fundamentals/sync/sync_test.go
@@ -6,9 +6,16 @@ func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
 		counter.Inc()
+		counter.Inc()
+		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter, 3)
 	})
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
 }
