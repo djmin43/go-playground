7414fa8b05f7565fe70403b86ef8b359d353c3e3
jay <dj.min43@gmail.com>
2023-09-17 07:37:55 +0900

    rest

---
+++ build-an-application/in_memory_player_store.go
@@ -0,0 +1,17 @@
+package main
+
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
+
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return i.store[name]
+}
--- build-an-application/main.go
+++ build-an-application/main.go
@@ -5,15 +5,6 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-}
-
 func main() {
 	server := &PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5002", server))
