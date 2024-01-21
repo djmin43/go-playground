ac54b2d065db7faab31c65cba2148bb6d0f614fb
dongjoon min <dj.min43@gmail.com>
2024-01-21 15:03:13 +0900

    add js and file systems

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -7,6 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
