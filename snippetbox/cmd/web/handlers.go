3f020755c06b9478afc3339ba1a735568cebe712
dongjoon min <dj.min43@gmail.com>
2024-01-21 11:20:35 +0900

    add template languages

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -10,6 +12,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
+	}
+
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// second argument is any dynamic data we want to pass in.
+	err = ts.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
 	w.Write([]byte("Hello from Snippetbox"))
