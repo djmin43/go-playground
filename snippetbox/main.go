3daf826ee5600dc11d56da3ffff01f5b07587e3b
dongjoon min <dj.min43@gmail.com>
2024-01-21 09:40:23 +0900

    query string

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Define a home handler function with writes a byte slice containing
@@ -20,12 +22,21 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
+		w.Header()["Date"] = nil
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		w.Write([]byte("Method Not Allowed"))
 		return
