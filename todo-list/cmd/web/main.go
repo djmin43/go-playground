d68193ed1694543cb5a53ea7c8d7bf1429991ff4
dongjoon min <dj.min43@gmail.com>
2024-01-21 21:21:51 +0900

    simple home handler

--- todo-list/cmd/web/main.go
+++ todo-list/cmd/web/main.go
@@ -7,8 +7,6 @@ import (
 
 func main() {
 
-	fmt.Println("hello")
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
@@ -18,6 +16,12 @@ func main() {
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
+		http.Error(w, "Get Method Required", http.StatusMethodNotAllowed)
+		return
+	}
 
 	fmt.Fprint(w, "hello world")
 }
