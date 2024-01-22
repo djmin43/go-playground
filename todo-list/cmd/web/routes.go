2ff50bd9d03c07df0310847f2544ae4f10ca2cb7
dongjoon min <dj.min43@gmail.com>
2024-01-22 21:38:04 +0900

    add file server

--- todo-list/cmd/web/routes.go
+++ todo-list/cmd/web/routes.go
@@ -4,12 +4,13 @@ import "net/http"
 
 // need to know why it has return func type here
 func (app *application) routes() *http.ServeMux {
-
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", app.home)
+	fileServer := http.FileServer(http.Dir("./ui/static"))
 
-	//Not sure how to add file server..
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
 
 	return mux
 }
