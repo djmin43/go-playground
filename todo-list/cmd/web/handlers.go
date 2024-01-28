098b1dbcd0fd571587916869188f6c6f6d3944a2
dongjoon min <dj.min43@gmail.com>
2024-01-28 18:47:20 +0900

    add helpers

--- todo-list/cmd/web/handlers.go
+++ todo-list/cmd/web/handlers.go
@@ -7,6 +7,11 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	if r.Method != http.MethodGet {
 		w.Header().Set("Allow", http.MethodGet)
 		http.Error(w, "Get Method Required", http.StatusMethodNotAllowed)
---
+++ todo-list/cmd/web/helpers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+
+}
