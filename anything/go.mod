6b5ceab12cbde8e2709392d7ac4f7db8c90c6f11
dongjoon min <dj.min43@gmail.com>
2024-01-28 19:06:24 +0900

    add anything

---
+++ anything/go.mod
@@ -0,0 +1,3 @@
+module anything
+
+go 1.21.0
---
+++ anything/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	age := 35
+	name := "mindongjoon"
+
+	//Print
+	fmt.Print("hello, \n")
+	fmt.Print("world\n")
+
+	// Println
+	fmt.Println("new line")
+	fmt.Println("new line again")
+	fmt.Println("my age is ", age, "and my name is", name)
+
+	// Printf (formatted strings) %_ = format specifier
+	fmt.Printf("my name is %v and my name is %v \n", age, name)
+	fmt.Printf("my name is %q and my name is %q\n", age, name)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("you scored %0.1f points\n", 225.55)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my name is %v and my name is %v \n", age, name)
+	fmt.Println("the saved string is: ", str)
+
+}
