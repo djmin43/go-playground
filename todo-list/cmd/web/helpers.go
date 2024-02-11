2538e5165518f915070521156b126986582edeef
dongjoon min <dj.min43@gmail.com>
2024-02-11 16:39:47 +0900

    use output over print

--- todo-list/cmd/web/helpers.go
+++ todo-list/cmd/web/helpers.go
@@ -7,8 +7,9 @@ import (
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
+	// debug.Stack() traces current goroutine
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Println(trace)
+	app.errorLog.Output(2, trace)
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
