1fd435581638c0fce7494ece6cb67cecc086cbd6
dongjoon min <dj.min43@gmail.com>
2024-02-18 21:11:11 +0900

    caching template files wip

--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/djmin43/snippetbox/internal/models"
+import (
+	"github.com/djmin43/snippetbox/internal/models"
+	"html/template"
+	"path/filepath"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+
+	if err != nil {
+		return nil, err
+	}
+
 }
