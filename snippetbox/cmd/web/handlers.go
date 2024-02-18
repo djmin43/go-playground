df0407fbdb8025df1e57d01efe240a57a8721508
dongjoon min <dj.min43@gmail.com>
2024-02-18 20:39:21 +0900

    use latest

--- snippetbox/cmd/web/handlers.go
+++ snippetbox/cmd/web/handlers.go
@@ -22,31 +22,28 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/partials/input.tmpl",
 	}
 
-	//files := []string{
-	//	"./ui/html/base.tmpl",
-	//	"./ui/html/pages/home.tmpl",
-	//	"./ui/html/partials/nav.tmpl",
-	//	"./ui/html/partials/input.tmpl",
-	//}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serveError(w, err)
+		return
+	}
 
-	//ts, err := template.ParseFiles(files...)
+	data := &templateData{
+		Snippets: snippets,
+	}
 
-	//if err != nil {
-	//	app.serveError(w, err)
-	//	return
-	//}
-	//
-	//err = ts.ExecuteTemplate(w, "base", nil)
-	//
-	//if err != nil {
-	//	app.serveError(w, err)
-	//}
+	err = ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serveError(w, err)
+	}
 
-	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
--- snippetbox/cmd/web/templates.go
+++ snippetbox/cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/djmin43/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
