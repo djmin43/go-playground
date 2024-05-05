c7e2c94e2ff556a3f8a8822b6ee9958d8f57dbf1
dongjoon min <dj.min43@gmail.com>
2024-05-05 09:16:15 +0900

    learn about constant and varaibles

--- go_tutorials/cmd/tutorial_1/main.go
+++ go_tutorials/cmd/tutorial_1/main.go
@@ -1,8 +1,32 @@
 // look for the entry function here. 'main'
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	var intNum int = 32767
+	fmt.Println(intNum)
+
+	var myString string = "Hello world"
+	fmt.Println(myString)
+
+	// this is length of byte. go uses UTF-8.
+	fmt.Println(len("test"))
+
+	// rune is data type representing a character
+	fmt.Println(utf8.RuneCountInString("f"))
+
+	var myRune rune = 'a'
+	fmt.Println(myRune)
+
+	var myBoolean bool = true
+
+	fmt.Println(myBoolean)
+
+	// can't change. no initial value
+	const myCont string = "const value"
+
 }
