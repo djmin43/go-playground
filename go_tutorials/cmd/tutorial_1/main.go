24cd9ae09dce6112c7fd9a30ec93c0eb2ca95296
dongjoon min <dj.min43@gmail.com>
2024-05-05 09:07:49 +0900

    another hello world

---
+++ go_tutorials/cmd/tutorial_1/main.go
@@ -0,0 +1,8 @@
+// look for the entry function here. 'main'
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
---
+++ go_tutorials/go.mod
@@ -0,0 +1,3 @@
+module github.com/djmin43/go_tutorials
+
+go 1.21.7
