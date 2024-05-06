6f6de92a50d9462af3bd846ff6b46fa7720b2239
dongjoon min <dj.min43@gmail.com>
2024-05-06 21:18:20 +0900

    pointers

--- go_tutorials/cmd/tutorial_1/main.go
+++ go_tutorials/cmd/tutorial_1/main.go
@@ -3,45 +3,37 @@ package main
 
 import "fmt"
 
-type gasEngine struct {
-	mpg     uint8
-	gallons uint8
-	owner
-	int
-}
-
-type electricEngine struct {
-	mpkwh uint8
-	kwh   uint8
-}
-
-type owner struct {
-	name string
-}
-
-func (e gasEngine) milesLeft() uint8 {
-	return e.gallons * e.mpg
-}
-
-func (e electricEngine) milesLeft() uint8 {
-	return e.kwh * e.mpkwh
-}
-
-type engine interface {
-	milesLeft() uint8
-}
-
-func canMakeIt(e engine, miles uint8) {
-	if miles <= e.milesLeft() {
-		fmt.Println("You can make it there!")
-	} else {
-		fmt.Println("Need to fuel up first!")
-	}
-}
-
 func main() {
-	var myEngine = gasEngine{mpg: 25, gallons: 33, owner: owner{name: "ff"}, int: 33}
-	var yourEngine = electricEngine{kwh: 4, mpkwh: 3}
-	canMakeIt(myEngine, 50)
-	canMakeIt(yourEngine, 50)
+	// store the pointer
+	// * this star is just a pointer type. new(int32) intiailazes the p value to a pointer.
+	var p *int32 = new(int32)
+
+	// this is value
+	var i int32
+
+	// ampersand is the reference to the memory address of the variable
+	p = &i
+	*p = 1
+
+	// This star sign has a different duty. this sends the value of the pointer value.
+	fmt.Printf("The value p points to is: %v", *p)
+	fmt.Printf("\nThe value if i is: %v", i)
+
+	var slice = []int{1, 2, 3}
+	var sliceCopy = slice
+	sliceCopy[2] = 4
+	fmt.Printf("\nThe value slice points to is: %v", slice)
+	fmt.Printf("\nThe value slice points to is: %v", sliceCopy)
+
+	var thing1 = [5]float64{1, 2, 3, 4, 5}
+	var result [5]float64 = square(&thing1)
+	fmt.Printf("\nThe result is: %v", result)
+}
+
+func square(thing2 *[5]float64) [5]float64 {
+	for i := range thing2 {
+		thing2[i] = thing2[i] * thing2[i]
+	}
+	return *thing2
+
 }
