8894ef30fbd35c2b1b0edadb2f7043a8835cf86f
dongjoon min <dj.min43@gmail.com>
2024-05-06 20:44:17 +0900

    strings

--- go_tutorials/cmd/tutorial_1/main.go
+++ go_tutorials/cmd/tutorial_1/main.go
@@ -1,48 +1,28 @@
 // look for the entry function here. 'main'
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	//var intArr [3]int32 = [...]int32{1, 2, 3}
-	intArr1 := [3]int32{1, 2, 3}
-	fmt.Println(&intArr1[1])
 
-	// Slices wrap arrays to give a more general, powerful, and convenient interface to sequences of data.
-
-	var someSlice = []int32{3, 4, 5, 6}
-	fmt.Printf("capacity of someSlice is %v", cap(someSlice))
-	someSlice = append(someSlice, 7)
-	fmt.Printf("capacity of someSlice is %v", cap(someSlice))
-
-	var intSlice3 []int32 = make([]int32, 3)
-	intSlice3 = append(intSlice3, 6)
-	fmt.Println(intSlice3)
-
-	var someMap map[string]uint8 = map[string]uint8{"Adam": 23, "Sarah": 45}
-	fmt.Println(someMap["Adam"])
-	fmt.Println(someMap["Json"])
-
-	// map does not guarantee the order
-	for name := range someMap {
-		fmt.Printf("Name: %v\n", name)
+	var myString = []rune("résumé")
+	for i, v := range myString {
+		fmt.Println(i, v)
 	}
 
-	for index, value := range intSlice3 {
-		fmt.Printf("someNumber: %v, %v\n", index, value)
-	}
+	// String is just byte. rune is the unicode point number
+	// rune i just alias for int32
+	var myRune = 'a'
+	fmt.Printf("\n my Rune = %v, %T", myRune, myRune)
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	var strSlice = []string{"s", "u", "b", "s", "c", "r", "i", "b", "e"}
+	var strBuilder strings.Builder
+	for i := range strSlice {
+		strBuilder.WriteString(strSlice[i])
 	}
-
-	var i int32 = 0
-	for {
-		if i >= 10 {
-			break
-		}
-		fmt.Println(i)
-		i++
-	}
-	fmt.Println("hello")
+	var catStr = strBuilder.String()
+	fmt.Println(catStr)
 }
