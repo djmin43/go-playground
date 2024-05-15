99983256ad431c7d07d94a007a7925372a8d5f70
dongjoon min <dj.min43@gmail.com>
2024-05-15 17:33:52 +0900

    create query

--- templates-practice/main.go
+++ templates-practice/main.go
@@ -2,27 +2,56 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"fmt"
 	"github.com/go-chi/chi/v5"
 	_ "github.com/mattn/go-sqlite3"
+	"html/template"
 )
 
 type Article struct {
-	ID      int    `json:"id333"`
-	Title   string `json:"title333"`
-	Content string `json:"content333"`
+	ID      int           `json:"id"`
+	Title   string        `json:"title"`
+	Content template.HTML `json:"content"`
 }
 
 var router *chi.Mux
 var db *sql.DB
 
 func main() {
-	article := Article{1, "title1", "content1"}
-	jsonData, err := json.Marshal(article)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(jsonData))
 
+}
+
+func dbCreateArticle(article *Article) error {
+	query, err := db.Prepare("insert into articles(title, content) values (?, ?)")
+	defer query.Close()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = query.Exec(article.Title, article.Content)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func connect() (*sql.DB, error) {
+	var err error
+	db, err = sql.Open("sqlite3", "./data.sqlite")
+
+	if err != nil {
+		return nil, err
+	}
+
+	sqlStmt := `
+    create table if not exists articles (id integer not null primary key autoincrement, title text, content text);
+    `
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
