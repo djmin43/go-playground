02bdf01c5e14c31d972bd3c005253fe534b8fa86
dongjoon min <dj.min43@gmail.com>
2024-05-26 21:35:14 +0900

    add worcount

--- go-class/file-io/main.go
+++ go-class/file-io/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, fname := range os.Args[1:] {
+		var lc, wc, cc int
+
 		file, err := os.Open(fname)
 
 		if err != nil {
@@ -15,12 +18,21 @@ func main() {
 			continue
 		}
 
-		if _, err := io.Copy(os.Stdout, file); err != nil {
-			fmt.Fprint(os.Stderr, err)
-			continue
+		scan := bufio.NewScanner(file)
+
+		for scan.Scan() {
+			s := scan.Text()
+
+			wc += len(strings.Fields(s))
+			cc += len(s)
+
+			lc++
+
 		}
 
+		fmt.Printf(" %7d %7d %7d %s\n", lc, wc, cc, fname)
 		file.Close()
+
 	}
 
 }
