e49b25ccb22c877ab6d9a7295b8d7f8aabe20005
dongjoon min <dj.min43@gmail.com>
2024-05-26 21:06:31 +0900

    bytes

--- go-class/file-io/main.go
+++ go-class/file-io/main.go
@@ -6,6 +6,8 @@ func main() {
 	s := []int{1, 2, 3}
 	a := [3]rune{'a', 'b', 'c'}
 	m := map[string]int{"and": 1, "or": 2}
+	t := "a string"
+	b := []byte(t)
 
 	fmt.Printf("%T\n", s)
 	fmt.Printf("%v\n", s)
@@ -18,5 +20,16 @@ func main() {
 	fmt.Printf("%T\n", m)
 	fmt.Printf("%v\n", m)
 	fmt.Printf("%#v\n", m)
+
+	fmt.Printf("%T\n", t)
+	fmt.Printf("%v\n", t)
+	fmt.Printf("%#v\n", t)
+
+	fmt.Println()
+
+	fmt.Printf("%T\n", b)
+	fmt.Printf("%v\n", b)
+	fmt.Printf("%q\n", b)
+	fmt.Printf("%#v\n", string(b))
 
 }
