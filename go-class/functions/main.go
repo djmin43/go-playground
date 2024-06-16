e7bd0e7c04a012d175d309dba13859c48944d42c
dongjoon min <dj.min43@gmail.com>
2024-06-16 22:14:47 +0900

    reference

--- go-class/functions/main.go
+++ go-class/functions/main.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
-func do(b []int) int {
-	b[0] = 0
-	fmt.Printf("b@ %p\n", b)
-	fmt.Printf("b@ %v\n", b)
-	return b[1]
+func do(m1 *map[int]int) {
+	(*m1)[3] = 0
+	fmt.Println("m1", *m1)
+	*m1 = make(map[int]int)
+	(*m1)[4] = 4
+	fmt.Println("m1", *m1)
 }
 
 func main() {
-	a := []int{1, 2, 3}
-	fmt.Printf("a@ %p\n", a)
-	fmt.Printf("a@ %v\n", a)
+	m := map[int]int{4: 1, 7: 2, 8: 3}
 
-	v := do(a)
+	fmt.Println("m", m)
 
-	fmt.Println(a, v)
+	do(&m)
+
+	fmt.Println("m", m)
 
 }
