efaa5d64bddbb320b5f83c57b34aa5f4d39c2b4e
dongjoon min <dj.min43@gmail.com>
2024-07-14 12:13:51 +0900

    connect to db

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -16,10 +18,20 @@ func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+	dsn := flag.String("dsn", "web:0000@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		infoLog:  infoLog,
@@ -33,7 +45,16 @@ func main() {
 	}
 
 	infoLog.Printf("Listening on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
