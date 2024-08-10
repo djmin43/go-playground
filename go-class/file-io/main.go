2e22805a111bc78adb364e05b06c5aee36f01058
dongjoon min <dj.min43@gmail.com>
2024-08-10 13:00:34 +0900

    file

--- go-class/file-io/main.go
+++ go-class/file-io/main.go
@@ -1,37 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
 	for _, fname := range os.Args[1:] {
-		var lc, wc, cc int
-
 		file, err := os.Open(fname)
 
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			continue
 		}
-
-		scan := bufio.NewScanner(file)
-
-		for scan.Scan() {
-			s := scan.Text()
-
-			wc += len(strings.Fields(s))
-			cc += len(s)
-
-			lc++
-
-		}
-
-		fmt.Printf(" %7d %7d %7d %s\n", lc, wc, cc, fname)
-		file.Close()
 
 	}
 
