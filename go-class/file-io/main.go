f534b9f93ab5c4eefc47aedc123ac4bddc4ae219
dongjoon min <dj.min43@gmail.com>
2024-08-15 19:30:10 +0900

    rss scraper

--- go-class/file-io/main.go
+++ go-class/file-io/main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
-
 func main() {
-	for _, fname := range os.Args[1:] {
-		file, err := os.Open(fname)
-
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			continue
-		}
-
-	}
 
 }
