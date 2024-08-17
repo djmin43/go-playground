35928a72318a1fe1c1d46c9aa45c9398b8a9b569
dongjoon min <dj.min43@gmail.com>
2024-08-17 14:23:21 +0900

    http

--- go-class/network-with-http/main.go
+++ go-class/network-with-http/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
 )
+
+var form = `
+<h1>Todo #{{.ID}}</h1>
+<div>{{ printf "User %d" .UserID }}</div>
+<div>{{ printf "%s (completed: %t)" .Title .Completed }}</div>
+
+`
+
+type todo struct {
+	UserID    int    `json:"userID"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
 
 func main() {
 	http.HandleFunc("/", handler)
@@ -13,5 +28,26 @@ func main() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! from %s\n", r.URL.Path[1:])
+	const base = "https://jsonplaceholder.typicode.com/"
+	resp, err := http.Get(base + r.URL.Path[1:])
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var item todo
+
+	if err = json.NewDecoder(resp.Body).Decode(&item); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tmpl := template.New("mine")
+
+	tmpl.Parse(form)
+	tmpl.Execute(w, item)
+
 }
