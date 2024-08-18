62f45639e3f4d7acb1073db2d93e6e00fa9c96d7
dongjoon min <dj.min43@gmail.com>
2024-08-18 21:08:09 +0900

    add point

--- go-class/interfaces/main.go
+++ go-class/interfaces/main.go
@@ -1,1 +1,31 @@
-package interfaces
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y float64
+}
+
+type Line struct {
+	Begin, End Point
+}
+
+func (l Line) Disatnce() float64 {
+	return math.Hypot(l.End.X-l.Begin.X, l.End.Y-l.Begin.Y)
+}
+
+func (l *Line) ScaleBy(f float64) {
+	l.End.X += (f - 1) * (l.End.X - l.Begin.X)
+	l.End.Y += (f - 1) * (l.End.Y - l.Begin.Y)
+
+}
+
+func main() {
+	side := Line{Point{1, 2}, Point{4, 6}}
+
+	side.ScaleBy(2)
+	fmt.Println(side.Disatnce())
+}
